2ac530129ecb71f3af870a9b77a786c05947a3e0
Rico Prasetya <ricoprasetya24@gmail.com>
2022-12-08 16:38:36 +0700

    menambah tugas

---
+++ Tugas/latihan4.py
@@ -0,0 +1,12 @@
+print("Nama : Rico Prasetya")
+print("NIM  : 312210425")
+print("")
+print("Program Menghitung Luas dan Keliling Lingkaran")
+print("")
+phi = 3.14
+r = float(input("Masukkan panjang jari-jari lingkaran: "))
+luas = phi*r*r
+keliling = 2*phi*r
+print("")
+print("Luas lingkaran adalah : "+ str(luas))
+print("Keliling lingkaran adalah : "+ str(keliling))
