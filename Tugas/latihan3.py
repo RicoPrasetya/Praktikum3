549d7ea498a0e5b037827cc28339a839d3da609e
Rico Prasetya <ricoprasetya24@gmail.com>
2022-12-08 14:37:46 +0700

    menambah tugas

--- Tugas/latihan3.py
+++ Tugas/latihan3.py
@@ -1,1 +1,17 @@
-# latihan3
+print("Nama : Rico Prasetya")
+print("NIM  : 312210425")
+print("")
+n=5
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ",end="")
+    for k in range(2*i+1):
+        print("*",end="")
+    print()
+n-=1
+for i in range(n):
+    for j in range(i+1):
+        print(" ",end="")
+    for j in range(2*(n-i)-1):
+        print("*",end="")
+    print()
